834b3f8488a6b5f2a46b0e81e2c83bd66b86362e
ressof <niklas@fosser.se>
2022-09-08 11:23:25 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -141,10 +141,10 @@ class BasePlugin:
                 self.CurrentPriceUpdated = False
 
 #            if HourNow >= 0 and MinuteNow >= 2 and MinuteNow < 59 and self.TodayPriceUpdated is False:
-            if MinuteNow == 21 and self.TodayPriceUpdated is False:
+            if MinuteNow == 24 and self.TodayPriceUpdated is False:
                 TodayPrice()
 
-            if MinuteNow == 22 and self.TodayPriceUpdated is True:
+            if MinuteNow == 25 and self.TodayPriceUpdated is True:
                 self.TodayPriceUpdated = False
 
 global _plugin
@@ -165,6 +165,7 @@ def TodayPrice():
             for each in price["areas"][_plugin.Area]["values"]:
                 if hour < HourNow:
                     FutureHour = FuturePrice(hour)
+                    Domoticz.Log("+++++++++"+FutureHour)
                     if FutureHour != 'inf':
                         Domoticz.Log("Hour "+str(hour)+" "+str(round(FutureHour/10.0,1)))
                         UpdateDevice(int(hour), FutureHour, str("Hour"+" "+str(hour)))
